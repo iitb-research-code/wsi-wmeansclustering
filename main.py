b7b3d475808af15a6577638df7e2f6e7d6c3a4e8
Venkatapathy Subramanian <venkat.s.iyer@gmail.com>
2023-03-04 17:40:05 +0530

    some pyc

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 import cv2
 import torch
 from sklearn.cluster import KMeans
-from utils import visualizeWeakbboxes, weakLabeling, visualizeIndividualClusterinDir
+from utils import visualizeWeakbboxes, weakLabeling, visualizeIndividualClusterinDir, saveimgsforYolo
 
 
 #load lysto dataset
@@ -26,47 +26,54 @@ if(os.path.isfile(lystoh5file)==False):
 ds=h5py.File(lystoh5file, 'r')
 selected_imgs = ds['x'][:numofimages,16:-16,16:-16,:]
 selected_labels=ds['y'][:numofimages]
-#if weaklabeling not done, please do
-if(os.path.isfile(susbseth5file)==False):
-    weakLabeling(selected_imgs,selected_labels)
-else:
-    weakLabeling(selected_imgs,selected_labels)
+
+for i in range(num_of_epochs):
+
+    #if weaklabeling not done, please do
+    if(os.path.isfile(susbseth5file)==False):
+        weakLabeling(selected_imgs,selected_labels)
+    else:
+        weakLabeling(selected_imgs,selected_labels)
 
 
-#read already create weaklabels
-newds=h5py.File(susbseth5file, 'r')
+    #read already create weaklabels
+    newds=h5py.File(susbseth5file, 'r')
 
-#visualize weaklabelings
-visualizeWeakbboxes()
+    #visualize weaklabelings
+    visualizeWeakbboxes()
 
-all_weak_bboxes=[]
-all_weak_features=[]
+    all_weak_bboxes=[]
+    all_weak_features=[]
 
-for i in newds['indROIs'].keys():
-    for j in newds['indROIs'][i]['bboxes'][:]:
-        all_weak_bboxes.append(j)
+    for i in newds['indROIs'].keys():
+        for j in newds['indROIs'][i]['bboxes'][:]:
+            all_weak_bboxes.append(j)
 
-for i in newds['indROIs'].keys():
-    for j in newds['indROIs'][i]['features'][:]:
-        all_weak_features.append(j)
+    for i in newds['indROIs'].keys():
+        for j in newds['indROIs'][i]['features'][:]:
+            all_weak_features.append(j)
 
 
-# Convert the list of feature vectors to a numpy array
-all_weak_features = np.array(all_weak_features)
-   
+    # Convert the list of feature vectors to a numpy array
+    all_weak_features = np.array(all_weak_features)
     
-# Perform K-means clustering on the feature vectors
-labels = KMeans(n_clusters=n_clusters, random_state=0).fit_predict(all_weak_features)
+        
+    # Perform K-means clustering on the feature vectors
+    labels = KMeans(n_clusters=n_clusters, random_state=0).fit_predict(all_weak_features)
 
-#save clusters for checking- visual
-visualizeIndividualClusterinDir(newds,labels)
+    #save clusters for checking- visual
+    visualizeIndividualClusterinDir(newds,labels)
 
-#savclusterimg(os.path.join(visualizationdir,'cluster_2'))
-#savclusterimg(os.path.join(visualizationdir,'cluster_3'))
+    #
+    print('There are ',n_clusters,' clusters.')
+    print('Each of them is save @,',visualizationdir)
+    selected_cluster_n=int(input('Choose one that best represents the group'))
 
+    #given images as x ROIS for each x as indROIs, labels in the same
+    #order save the images and bboxes for yolo training
+    saveimgsforYolo(selected_cluster_n,labels)
 
+    #train on yolo
 
+    #infer on yolo
 
-
-#clustering
-#afterclustering(selected_images_bboxes)
